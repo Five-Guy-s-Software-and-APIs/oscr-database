eb1cdc18625e7507c0bff144a84762552b8345f5
Emmanuel Castrejon <ecastrejon@csus.edu>
2021-03-31 13:44:05 -0700

    Began incorporating search with Kaggle dataset

--- application/routes/routes.js
+++ application/routes/routes.js
@@ -6,6 +6,10 @@ var appRouter = function (app) {
     app.get("/search", function(req, res) {
         res.status(200).send("Welcome to the search");
     });
+
+    app.get("/index", function(req, res) {
+      res.status(200).sendFile('/index.html', {root: __dirname})
+    });
   }
   
   module.exports = appRouter;
