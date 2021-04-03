c8b104ddb1450afb8b614783d68f6d3bb6d191a0
Emmanuel Castrejon <ecastrejon@csus.edu>
2021-04-02 18:40:19 -0700

    Fixed routes.js

--- application/app.js
+++ application/app.js
@@ -1,26 +1,11 @@
 const express = require("express");
-const data = require("./data.json");
-//const routes = require("./routes/routes.js");
+const routes = require("./routes/routes.js");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.status(200).sendFile('/index.html', {root: __dirname})
-});
-
-app.get("/search", (req, res) => {
-    //res.status(200).send(req.query);
-    var result = [];
-    for(const nominee of data) {
-        if(parseInt(nominee.year_film) === parseInt(req.query.year)) {
-            result.push(nominee);
-        }
-    }
-
-    res.status(200).send(JSON.stringify(result));
-});
+routes(app);
 
 const port = process.env.PORT || 3000;
 
--- application/routes/routes.js
+++ application/routes/routes.js
@@ -1,15 +1,21 @@
+const path = require("path");
+const data = require(path.resolve('data.json'));
+
 var appRouter = function (app) {
-    /*app.get("/", function(req, res) {
-      res.status(200).send("Welcome to our restful API");
-    });*/
-
-    app.get("/", (req, res) => {
-      res.status(200).sendFile('/index.html', {root: __dirname})
-    });
-
-    app.get("/search", (req, res) => {
-      res.send(req.query);
+  app.get("/", (req, res) => {
+    res.status(200).sendFile(path.resolve('public/index.html'));
   });
-  }
+
+  app.get("/year", (req, res) => {
+    let result = [];
+    for(const nominee of data) {
+        if(parseInt(nominee.year_film) === parseInt(req.query.year)) {
+            result.push(nominee);
+        }
+    }
+
+    res.status(200).json(result);
+  });
+}
   
   module.exports = appRouter;
