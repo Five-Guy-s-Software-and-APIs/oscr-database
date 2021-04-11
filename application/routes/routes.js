6e5bea519633cb3601148a6b5161f601d3df91e2
iamdeeshadow <80650349+iamdeeshadow@users.noreply.github.com>
2021-04-10 18:33:45 -0700

    added /movies

--- application/routes/routes.js
+++ application/routes/routes.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const data = require(path.resolve('data.json'));
 const search = require('../lib/search');
+const match = require('../lib/matchByParameter');
 
 var appRouter = function (app) {
   app.get("/", (req, res) => {
@@ -9,23 +10,62 @@ var appRouter = function (app) {
 
   app.get("/search", (req, res) => {
     let result = data.filter(nominee => {
-      return ((!req.query.year || (parseInt(nominee.year_film) == parseInt(req.query.year)))
-              && (!req.query.name || nominee.name.toUpperCase().includes(req.query.name.toUpperCase()))
-              && (!req.query.category || nominee.category.includes(req.query.category.toUpperCase()))
-              && (!req.query.winner || nominee.winner)
-              && (!req.query.film || nominee.film.toUpperCase().includes(req.query.film.toUpperCase()) || nominee.name.toUpperCase().includes(req.query.film.toUpperCase())));
+      return match.matchByParameter(req.query, nominee);
     });
 
-    let html = search.formatSearchResult(result);
+    res.status(200).json(result);
+    //let html = search.formatSearchResult(result);
 
-    res.status(200).set('Content-Type', 'text/html').send(html);
+    //res.status(200).set('Content-Type', 'text/html').send(html);
   });
 
+  app.get("/movies", (req, res) => {
+    res.send (data);
+  });
 
+  app.get("/categories/:category*", (req, res, next) => {
+    let db = undefined;
+    if(res.hasOwnProperty("result")) {
+      db = res.result;
+    } else {
+      db = data;
+    }
+    res.result = db.filter(nominee => {
+      return match.matchByParameter(req.params, nominee);
+    });
+    next();
+  })
 
+  app.get("/year/:year_film*", (req, res, next) => {
+    let db = undefined;
+    if(res.hasOwnProperty("result")) {
+      db = res.result;
+    } else {
+      db = data;
+    }
+    res.result = db.filter(nominee => {
+      return match.matchByParameter(req.params, nominee);
+    });
+    next();
+  })
 
+  app.get("/*", (req, res, next) => {
+    if(res.hasOwnProperty("result")) {
+      res.status(200).json(res.result);
+    } else {
+      next();
+    };
 
+  })
 
+  app.get("/public/searchbar", (req, res) => {
+    res.status(200).sendFile(path.resolve('public/searchbar.html'));
+  });
+
+  app.get("/public/js/index", (req, res) => {
+    res.status(200).sendFile(path.resolve('public/js/index.js'))
+  })
+  
 }
 
   module.exports = appRouter;
