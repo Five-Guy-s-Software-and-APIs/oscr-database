46c2b2dea49e0a3d78ced6064e7a7ccc098b0386
Emmanuel Castrejon <ecastrejon@csus.edu>
2021-05-06 12:51:07 -0700

    Separated script from html into its own file

--- application/public/js/index.js
+++ application/public/js/index.js
@@ -1,14 +1,29 @@
 $(document).ready(function() {
-    $("#searchbar").load("/public/searchbar");
-
-    // $("#sub_button").click(function(){
-    //     $.get("/search", {
-    //       year_film : document.getElementById("fyear").value,
-    //       name : document.getElementById("name").value,
-    //       category : document.getElementById("category").value,
-    //       winner : document.getElementById("winner")
-    //     }, (data) => {
-    //         $(document).load(search.formatSearchResult(data));
-    //     });
-    // });
+    $("#sub_button").click(function(){
+        $.get("/search",
+          {
+          year_film : document.getElementById("fyear").value,
+          name : document.getElementById("name").value,
+          category : document.getElementById("category").value,
+          winner : document.getElementById("winner").checked,
+        },
+    
+          function(data, status){
+          dispResults(data);
+        });
+      });
 })
+
+function dispResults(db) {
+    var table = "<tr><th>Category</th><th>Name</th><th>Film</th><th>Year</th><th>Winner</th></tr>";
+    for(var i=0; i<db.length; ++i) {
+        table += "<tr>";
+        table += "<td> " + String(db[i]["category"]) + " </td>";
+        table += "<td> " + String(db[i]["name"]) + " </td>";
+        table += "<td> " + String(db[i]["film"]) + " </td>";
+        table += "<td> " + String(db[i]["year_film"]) + " </td>";
+        table += "<td> " + String(db[i]["winner"]) + " </td>";
+        table += "</tr>";
+    }
+    document.getElementById('output_table').innerHTML  = table;
+}  
