154a51a60ab71c1f993509fe36790e4993727baa
Emmanuel Castrejon <ecastrejon@csus.edu>
2021-05-09 14:03:26 -0700

    Added ability to search by multiple categories in client code TODO: Clean up interface

--- application/public/js/index.js
+++ application/public/js/index.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-    $("#sub_button").click(function(){
-        $.get("/search",
-          {
-          year_film : document.getElementById("fyear").value,
-          name : document.getElementById("name").value,
-          category : document.getElementById("category").value,
-          winner : document.getElementById("winner").checked,
-        },
-    
-          function(data, status){
-          dispResults(data);
-        });
-      });
+  $('#search').submit(function(e) {
+    e.preventDefault
+    let url = $(this).attr('action');
+    $.ajax({
+      type: 'GET',
+      url: url,
+      data: $(this).serialize(),
+      success: function(response) {
+        dispResults(response);
+      }
+    });
+    return false
+  });
 })
 
 function dispResults(db) {
@@ -26,4 +26,26 @@ function dispResults(db) {
         table += "</tr>";
     }
     document.getElementById('output_table').innerHTML  = table;
-}  
+}
+
+function addFields(){
+  var container = document.getElementById("container");
+    // Append a node with a random text
+    var a = document.createElement("a");
+
+    container.appendChild(a.appendChild(document.createTextNode("Category")));
+    // Create an <input> element, set its type and name attributes
+    var input = document.createElement("input");
+    input.type = "text";
+    input.name = "category";
+    container.appendChild(input);
+    // Append a line break 
+    container.appendChild(document.createElement("br"));
+}
+
+function clearFields() {
+  var container = document.getElementById("container");
+  while (container.hasChildNodes()) {
+    container.removeChild(container.lastChild);
+  }
+}
