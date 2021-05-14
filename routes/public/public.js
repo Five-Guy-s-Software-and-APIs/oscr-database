561800351dd1ac04d596e581bf6e7b2639c5730e
Emmanuel Castrejon <ecastrejon@csus.edu>
2021-05-13 18:30:33 -0700

    added logo to header

--- routes/public/public.js
+++ routes/public/public.js
@@ -2,9 +2,8 @@ const express = require("express");
 const path = require("path");
 let router = express.Router();
 
-
-router.get("/searchbar", (req, res) => {
-    res.status(200).sendFile(path.resolve('public/searchbar.html'));
+router.get("/logo", (req, res) => {
+    res.status(200).sendFile(path.resolve('public/logo_2.png'));
 });
 
 router.get("/js/index", (req, res) => {
@@ -14,6 +13,10 @@ router.get("/js/index", (req, res) => {
 router.get("/mystyles", (req, res) => {
     res.sendFile(path.resolve('public/mystyles.css'));
 });
+
+router.get("/header", (req,res) => {
+    res.sendFile(path.resolve('public/header.css'));
+})
   
 
 module.exports = router;
