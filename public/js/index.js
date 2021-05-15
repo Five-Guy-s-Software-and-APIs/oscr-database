3957b818779168d76a7a0adcfa812a4b39f1f564
Emmanuel Castrejon <ecastrejon@csus.edu>
2021-05-14 17:37:25 -0700

    Made reset button only visible when additional categories present

--- public/js/index.js
+++ public/js/index.js
@@ -1,52 +1,56 @@
-$(document).ready(function() {
-  $('#search').submit(function(event) {
+$(document).ready(function () {
+  $("#search").submit(function (event) {
     event.preventDefault();
     $.ajax({
-      type: 'GET',
-      url: $(this).attr('action'),
+      type: "GET",
+      url: $(this).attr("action"),
       data: $(this).serialize(),
-      success: function(response) {
+      success: function (response) {
         dispResults(response);
       },
-      error: function(jqXHR, textStatus, errorThrown) {
-        document.getElementById('output_table').appendChild(document.createTextNode("Nothing found matching search query"));
-      }
+      error: function (_jqXHR, _textStatus, _errorThrown) {
+        document.getElementById("output_table").innerHTML =
+          "Nothing found matching search query";
+      },
     });
   });
-})
+});
 
 function dispResults(db) {
-    let table = "<tr><th scope=\"category\">Category</th><th scope=\"name\">Name</th><th scope=\"film\">Film</th><th scope=\"year\">Year</th><th scope=\"winner\">Winner</th></tr>";
-    for(let item of db) {
-        table += "<tr>";
-        table += "<td> " + String(item["category"]) + " </td>";
-        table += "<td> " + String(item["name"]) + " </td>";
-        table += "<td> " + String(item["film"]) + " </td>";
-        table += "<td> " + String(item["year_film"]) + " </td>";
-        table += "<td> " + String(item["winner"]) + " </td>";
-        table += "</tr>";
-    }
-    document.getElementById('output_table').innerHTML = table;
+  let table =
+    '<tr><th scope="category">Category</th><th scope="name">Name</th><th scope="film">Film</th><th scope="year">Year</th><th scope="winner">Winner</th></tr>';
+  for (let item of db) {
+    table += "<tr>";
+    table += "<td> " + String(item["category"]) + " </td>";
+    table += "<td> " + String(item["name"]) + " </td>";
+    table += "<td> " + String(item["film"]) + " </td>";
+    table += "<td> " + String(item["year_film"]) + " </td>";
+    table += "<td> " + String(item["winner"]) + " </td>";
+    table += "</tr>";
+  }
+  document.getElementById("output_table").innerHTML = table;
 }
 
 let extraInput = 1;
 
-function addFields(){
+function addFields() {
   var container = document.getElementById("container");
-    // Append a node with a random text
-    extraInput++;
-    var label = document.createElement("label");
-    label.setAttribute("for", `category${extraInput}`);
-    label.innerHTML = `Category ${extraInput}`;
-    container.appendChild(label);
-    // Create an <input> element, set its type and name attributes
-    var input = document.createElement("input");
-    input.type = "text";
-    input.name = `category${extraInput}`;
-    input.placeholder = "Category";
-    container.appendChild(input);
-    // Append a line break 
-    container.appendChild(document.createElement("br"));
+  // Append a node with a random text
+  extraInput++;
+  var label = document.createElement("label");
+  label.setAttribute("for", `category${extraInput}`);
+  label.innerHTML = `Category ${extraInput}`;
+  container.appendChild(label);
+  // Create an <input> element, set its type and name attributes
+  var input = document.createElement("input");
+  input.type = "text";
+  input.name = `category${extraInput}`;
+  input.placeholder = "Category";
+  container.appendChild(input);
+  // Append a line break
+  container.appendChild(document.createElement("br"));
+
+  document.getElementById("cleardetails").style.display = "block";
 }
 
 function clearFields() {
@@ -54,5 +58,6 @@ function clearFields() {
   while (container.hasChildNodes()) {
     container.removeChild(container.lastChild);
   }
+  document.getElementById("cleardetails").style.display = "none";
   extraInput = 1;
 }
